508f79586697d907065d52f1652892a2d8dbe01d
PK <Peks_96@live.no>
2018-02-05 16:01:01 +0100

    renaming folder

---
+++ Assets/_Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	[SerializeField]
+	private Vector2 CameraCage = new Vector2();
+	[SerializeField]
+	private float Speed;
+
+	private GameObject[] Players;
+
+	void Start () {
+		Players = FindObjectsOfType ("ActionableActioner");
+	}
+
+	void LateUpdate () {
+		
+	}
+
+	void Move(Vector3 direction){
+		
+	}
+
+	void FindMove(){
+		var PlayerSquare = new List <Vector2> (2); //left corner 0, right corner 1
+		for (int i = 0; i < Players.Length; i++) {
+			
+		}
+	}
+}
