9b066e65958474f64a5e65c4e7e17327fe44ed5d
Vífill Valdimarsson <vifill12@gmail.com>
2018-02-05 17:01:48 +0100

    Beginning of HealthController

---
+++ Assets/Scripts/PatientScripts/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+    public float CholeraSeverity;
+    public float HydrationMeter;
+
+    public float DehydrationSpeed;
+
+    private PatientStatusController PatientStatusController;
+
+    private void Start()
+    {
+        PatientStatusController = GetComponent<PatientStatusController>();
+    }
+
+    private void Update()
+    {
+        if(!PatientStatusController.IsDead)
+        {
+            HydrationMeter -= DehydrationSpeed * Time.deltaTime;
+
+            if (!PatientStatusController.IsDead && HydrationMeter <= 0)
+            {
+                PatientStatusController.Death();
+            }
+        }
+    }
+}
