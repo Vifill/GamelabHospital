34cc9ee564e9ab290b2706659c42b3b15cbd555e
Theodor Te Maiharoa <tedditm@gmail.com>
2018-02-05 16:09:53 +0100

    new scene and prefab for the sanitation bar

---
+++ Assets/Scripts/SanitationController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SanitationController : MonoBehaviour {
+
+    private Image SanitationProgressBar;
+    private float SanitationLevel;
+
+    public GameObject SanitationBarPrefab;
+
+	// Use this for initialization
+	private void Start() 
+	{
+        SanitationProgressBar = SanitationBarPrefab.transform.GetChild(0).GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	private void Update() 
+	{
+		
+	}
+}
