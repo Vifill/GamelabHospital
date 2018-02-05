7caca70841e3baf5100553516b2906e1451ba6a5
Theodor Te Maiharoa <tedditm@gmail.com>
2018-02-05 17:10:01 +0100

    continuing the sanitation controller

--- Assets/Scripts/SanitationController.cs
+++ Assets/Scripts/SanitationController.cs
@@ -5,22 +5,28 @@ using UnityEngine.UI;
 
 public class SanitationController : MonoBehaviour
 {
-
+    private GameObject SanitationBar;
     private Image SanitationProgressBar;
+    private Transform MainCanvas;
     public float CurrentSanitationLevel;
     public float MaxSanitationLevel;
 
     public GameObject SanitationBarPrefab;
+    public Transform ProgressBarWorldPosition;
+    public float UIOffset;
 
 	// Use this for initialization
 	private void Start() 
 	{
-        SanitationProgressBar = SanitationBarPrefab.transform.GetChild(0).GetComponent<Image>();
+        MainCanvas = GameObject.FindGameObjectWithTag("MainCanvas").transform;
+        SanitationBar = Instantiate(SanitationBarPrefab, MainCanvas);
+        SanitationProgressBar = SanitationBar.transform.GetChild(0).GetComponent<Image>();
 	}
 	
 	// Update is called once per frame
 	private void Update() 
 	{
         SanitationProgressBar.fillAmount = CurrentSanitationLevel / MaxSanitationLevel;
-	}
+        SanitationBar.transform.position = Camera.main.WorldToScreenPoint(ProgressBarWorldPosition.position) + new Vector3(0, UIOffset, 0);
+    }
 }
