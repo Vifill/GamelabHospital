a7d782161bad6e3edbbfc64e1233ed2042753835
Sindri Freyr <sindridi@gmail.com>
2018-02-05 17:01:20 +0100

    started working on a bed station script

---
+++ Assets/Scripts/Actions/Stations/BedStation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedStation : Actionable 
+{
+    //public bool IsDirty { private set; get; }
+    public bool IsDirty;
+    private BedController BedController;
+
+    protected override void Initialize()
+    {
+        BedController = GetComponent<BedController>();
+        IsDirty = false;
+    }
+
+    private void Update()
+    {
+        if (BedController.HasPatient)
+        {
+            IsActionActive = false;
+        }
+    }
+
+    public override bool CanBeActioned(ToolName pCurrentTool, GameObject pObjectActioning)
+    {
+        if (IsDirty && !BedController.IsReserved)
+        {
+            return true;
+        }
+        else if (!IsDirty)
+        {
+            return false;
+        }
+
+        return false;
+    }
+
+    public override void OnFinishedAction(GameObject pObjectActioning)
+    {
+        SetClean();
+    }
+
+    public void SetDirty()
+    {
+        IsDirty = true;
+    }
+
+    public void SetClean()
+    {
+        IsDirty = false;
+    }
+}
