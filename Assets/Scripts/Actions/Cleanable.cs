e7bf2e6776cb8475fd7ab0f29efb376f2f45b7b4
Sindri Freyr <sindridi@gmail.com>
2018-02-06 14:21:37 +0100

    Cleanable script

---
+++ Assets/Scripts/Actions/Cleanable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cleanable : Actionable
+{
+    private Actionable OtherActionable;
+
+    public bool IsDirty { private set; get; }
+    public ToolName RequiredTool;
+
+    protected override void Initialize()
+    {
+        var actionables = GetComponents<Actionable>();
+
+        if (actionables.Length > 1)
+        {
+            foreach (var actionable in actionables)
+            {
+                if (actionable != this)
+                {
+                    OtherActionable = actionable;
+                }
+            }
+        }
+    }
+
+    public override bool CanBeActioned(ToolName pCurrentTool, GameObject pObjectActioning)
+    {
+        if (IsDirty && pCurrentTool == RequiredTool)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public override void OnFinishedAction(GameObject pObjectActioning)
+    {
+        SetClean();
+    }
+
+    public void SetDirty()
+    {
+        IsDirty = true;
+        if (OtherActionable != null)
+        {
+            OtherActionable.IsActionActive = false;
+        }
+        
+    }
+
+    public void SetClean()
+    {
+        IsDirty = false;
+        if (OtherActionable != null)
+        {
+            OtherActionable.IsActionActive = true;
+        }
+    }
+}
