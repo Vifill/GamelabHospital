9b9221df4b31e04cf9abce51fb865394b696d5e6
Sindri Freyr <sindridi@gmail.com>
2018-02-09 16:48:43 +0100

    made a temporary shitty UI bar to display the beds dirtiness

--- Assets/_Scripts/Actions/Stations/BedStation.cs
+++ Assets/_Scripts/Actions/Stations/BedStation.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BedStation : Actionable 
 {
@@ -9,9 +10,28 @@ public class BedStation : Actionable
 
     public ToolName RequiredTool;
 
+    // UI Stuff
+    public GameObject DirtyBarPrefab;
+    private GameObject DirtyBarInstance;
+    private Camera Cam;
+    private float BarMaxWidth;
+    private float BarHeight;
+    private Image BarFill;
+
+
     protected override void Initialize()
     {
         BedController = GetComponent<BedController>();
+
+        //UI Stuff
+        if (DirtyBarPrefab != null)
+        Cam = FindObjectOfType<Camera>();
+        DirtyBarInstance = Instantiate(DirtyBarPrefab, FindObjectOfType<Canvas>().transform);
+        DirtyBarInstance.transform.position = Cam.WorldToScreenPoint(transform.position);
+        BarFill = DirtyBarInstance.transform.GetChild(0).GetComponent<Image>();
+        BarMaxWidth = BarFill.rectTransform.rect.width;
+        BarHeight = BarFill.rectTransform.rect.height;
+        UpdateDirtyUI();
     }
 
     public override bool CanBeActioned(ToolName pCurrentTool, GameObject pObjectActioning)
@@ -35,10 +55,19 @@ public class BedStation : Actionable
     {
         DirtyMeter += pValue;
         Mathf.Clamp(DirtyMeter, 0, 100);
+        //UI Stuff
+        UpdateDirtyUI();
     }
 
     public void SetClean()
     {
         DirtyMeter = 0;
+        //UI Stuff
+        UpdateDirtyUI();
+    }
+
+    private void UpdateDirtyUI()
+    {
+        BarFill.rectTransform.sizeDelta = new Vector2(((BarMaxWidth / 100) * DirtyMeter), BarHeight);
     }
 }
