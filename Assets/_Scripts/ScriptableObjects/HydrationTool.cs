9bdc6d738a1cf7a941ae5d2e70378ad69c834a1a
Sindri Freyr <sindridi@gmail.com>
2018-02-12 17:05:21 +0100

    A base for the hydration tool scripts

---
+++ Assets/_Scripts/ScriptableObjects/HydrationTool.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class HydrationTool : ScriptableObject
+{
+    public float HydrationReplenished;
+    public float TimeItTakes;
+
+    protected float Counter;
+
+    public abstract void UpdateTool(HealthController pHealthCtrl) { }
+}
