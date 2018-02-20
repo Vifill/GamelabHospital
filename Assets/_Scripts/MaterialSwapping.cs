f8c729366e39b99ae73a3deeae4bf216a7998f35
Per Kristian Sørensen <Peks_96@live.no>
2018-02-20 16:01:26 +0100

    shader work

---
+++ Assets/_Scripts/MaterialSwapping.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialSwapping : MonoBehaviour {
+
+	private ParticleSystem ParticleSystem;
+    private ParticleSystem.EmissionModule EmissionModule;
+    private ParticleSystemRenderer ParticleSystemRenderer;
+    private float Rate = 0;
+    private float Timer = 0;
+    private int Index = 0;
+    public Material[] Materials;
+
+	void Start () {
+        ParticleSystemRenderer = GetComponent<ParticleSystemRenderer>();
+        ParticleSystem = GetComponent<ParticleSystem>();
+        EmissionModule = ParticleSystem.emission;
+        Rate = 1/EmissionModule.rateOverTime.constant;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Timer += Time.deltaTime;
+        if (Timer >= Rate)
+        {
+
+        }
+	}
+
+    void SwapMaterial()
+    {
+        ParticleSystemRenderer.material = Materials[Index];
+        Index++;
+    }
+}
