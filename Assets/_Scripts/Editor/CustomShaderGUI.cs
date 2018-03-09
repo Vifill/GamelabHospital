1f7d4f2b8650610485987933a15cc6b6a1b459a2
Per Kristian Sørensen <Peks_96@live.no>
2018-03-09 16:40:47 +0100

    Brightness on WEBGL outline is a thing

---
+++ Assets/_Scripts/Editor/CustomShaderGUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class CustomShaderGUI : ShaderGUI
+{
+    public override void OnGUI(MaterialEditor materialEditor, MaterialProperty[] properties)
+    {
+        // render the default gui
+        base.OnGUI(materialEditor, properties);
+
+        Material targetMat = materialEditor.target as Material;
+
+        // see if redify is set, and show a checkbox
+        bool fluid = Array.IndexOf(targetMat.shaderKeywords, "FLUID_ON") != -1;
+        EditorGUI.BeginChangeCheck();
+        fluid = EditorGUILayout.Toggle("Redify material", fluid);
+        if (EditorGUI.EndChangeCheck())
+        {
+            // enable or disable the keyword based on checkbox
+            if (fluid)
+                targetMat.EnableKeyword("FLUID_ON");
+            else
+                targetMat.DisableKeyword("FLUID_ON");
+        }
+    }
+}
