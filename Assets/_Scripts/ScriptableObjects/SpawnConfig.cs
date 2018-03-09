2e3d644d3df26eb69c3d309a5015a2790e3437d5
Sindri Freyr <sindridi@gmail.com>
2018-03-09 15:26:50 +0100

    Made a duplicate of Vífills level and fixed it up a bit, made new configs to make it easier and deleted some unnecessary stuff

--- Assets/_Scripts/ScriptableObjects/SpawnConfig.cs
+++ Assets/_Scripts/ScriptableObjects/SpawnConfig.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu]
+[CreateAssetMenu(menuName = "Configs/Levels/SpawnConfig")]
 public class SpawnConfig : ScriptableObject
 {
     public float SpawnRate;
