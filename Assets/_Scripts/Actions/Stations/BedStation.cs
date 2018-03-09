85939e67be36be86c2311cf1baaf67edb94570e0
Theodor Te Maiharoa <tedditm@gmail.com>
2018-03-09 17:37:39 +0100

    buckets are in but trying to fix the offset bug

--- Assets/_Scripts/Actions/Stations/BedStation.cs
+++ Assets/_Scripts/Actions/Stations/BedStation.cs
@@ -71,4 +71,13 @@ public class BedStation : Actionable
     {
         DirtyBarInstance.transform.position = Cam.WorldToScreenPoint(transform.position);
     }
+
+    //public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z)
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+    //    Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
+    //    float distance;
+    //    xy.Raycast(ray, out distance);
+    //    return ray.GetPoint(distance);
+    //}
 }
