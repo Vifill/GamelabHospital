25996683f4cd13dcb6fb90fd22f337cec361251b
Per Kristian Sørensen <Peks_96@live.no>
2018-03-12 15:33:03 +0100

    Fluid Shader Implemented

---
+++ Assets/_Scripts/UI/UIFillAmount.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFillAmount : MonoBehaviour{
+
+    public Material SourceMaterial;
+    private Material Material;
+    private Image Image;
+    private float pFillAmount;
+    public float FillAmount
+    {
+        set
+        {
+            SetFillAmount(value);
+            pFillAmount = value;
+        }
+        get { return pFillAmount; }
+    }
+
+	void Start () {
+        Image = GetComponent<Image>();
+        Material = new Material(SourceMaterial);
+        if (Image != null)
+        {
+            Image.material = Material;
+        }
+    }
+	
+	void SetFillAmount(float pFill)
+    {
+        MaterialPropertyBlock props = new MaterialPropertyBlock();
+        if (Image != null)
+        {
+            Image.material.SetFloat("_BucketFillAmount", pFill);
+        }
+        else
+        {
+            print("<color=yellow> Material not found! </color>");
+        }
+    }
+}
