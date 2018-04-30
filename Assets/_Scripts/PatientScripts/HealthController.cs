4188d12ba868b8a44d4db911054866a33a03e6d4
Theodor Te Maiharoa <tedditm@gmail.com>
2018-04-30 14:34:46 +0200

    bucket animation fix

--- Assets/_Scripts/PatientScripts/HealthController.cs
+++ Assets/_Scripts/PatientScripts/HealthController.cs
@@ -197,6 +197,7 @@ public class HealthController : MonoBehaviour
             HydrationUI.GetComponent<HydrationUIManager>().SetExcreteWarning(false);
             // puke animation trigger
             PatientAnimator.SetTrigger(Constants.AnimationParameters.PatientPuke);
+            PatientPrefabAnimator.SetTrigger(Constants.AnimationParameters.PatientPuke);
             Debug.Log($"I'M PUKING!");
         }
         StartCoroutine(SickCoroutine());
