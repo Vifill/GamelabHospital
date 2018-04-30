89cf6b5e375a184c0ea6175e4e7815f411ba5291
Theodor Te Maiharoa <tedditm@gmail.com>
2018-04-30 16:29:48 +0200

    more tutorial stuff

--- Assets/_Scripts/ScriptableObjects/CoroutineStartClasses/TutorialCoroutineStartLogic_2.cs
+++ Assets/_Scripts/ScriptableObjects/CoroutineStartClasses/TutorialCoroutineStartLogic_2.cs
@@ -7,14 +7,14 @@ public class TutorialCoroutineStartLogic_2 : TutorialCoroutineStartLogic
     public override IEnumerator TutorialStartCoroutine()
     {
         TutorialUtility.SetTimeFreeze(true);
-        TutorialUtility.SetTimerUIAsActive(false);
+        //TutorialUtility.SetTimerUIAsActive(false);
 
-        yield return new WaitForEndOfFrame();
+        yield return new WaitForSeconds(0.1f);
 
         TutorialUtility.SetBedSanitationFreeze(false);
         TutorialUtility.SetSpawnFreeze(true);
-        TutorialUtility.SetPatientHydration(100);
-        TutorialUtility.SetPatientHealth(60);
+        TutorialUtility.SetPatientHydration(50);
+        TutorialUtility.SetPatientHealth(50);
         TutorialUtility.SetHydrationFreeze(true);
         TutorialUtility.SetHealthFreeze(false);
         TutorialUtility.SetFreezeExcretion(true);
