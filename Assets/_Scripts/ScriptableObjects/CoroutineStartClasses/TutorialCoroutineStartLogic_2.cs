d93fb4fd1e7b2af22b2d0c586a2b8323047ecc97
Theodor Te Maiharoa <tedditm@gmail.com>
2018-04-30 15:12:20 +0200

    some start on tutorial level2

--- Assets/_Scripts/ScriptableObjects/CoroutineStartClasses/TutorialCoroutineStartLogic_2.cs
+++ Assets/_Scripts/ScriptableObjects/CoroutineStartClasses/TutorialCoroutineStartLogic_2.cs
@@ -6,6 +6,17 @@ public class TutorialCoroutineStartLogic_2 : TutorialCoroutineStartLogic
 {
     public override IEnumerator TutorialStartCoroutine()
     {
-        return base.TutorialStartCoroutine();
+        TutorialUtility.SetTimeFreeze(true);
+        TutorialUtility.SetTimerUIAsActive(false);
+
+        yield return new WaitForEndOfFrame();
+
+        TutorialUtility.SetBedSanitationFreeze(false);
+        TutorialUtility.SetSpawnFreeze(true);
+        TutorialUtility.SetPatientHydration(100);
+        TutorialUtility.SetPatientHealth(60);
+        TutorialUtility.SetHydrationFreeze(true);
+        TutorialUtility.SetHealthFreeze(false);
+        TutorialUtility.SetFreezeExcretion(true);
     }
 }
