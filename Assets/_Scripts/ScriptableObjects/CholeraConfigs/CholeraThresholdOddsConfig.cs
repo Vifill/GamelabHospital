b08184f0a02181cf1a59126adb86a0c064255837
Vífill Valdimarsson <vifill12@gmail.com>
2018-05-01 12:33:59 +0200

    Cleaning a little

--- Assets/_Scripts/ScriptableObjects/CholeraConfigs/CholeraThresholdOddsConfig.cs
+++ Assets/_Scripts/ScriptableObjects/CholeraConfigs/CholeraThresholdOddsConfig.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 [CreateAssetMenu(menuName = "Configs/Patient/CholeraThresholdOddsConfig")]
 public class CholeraThresholdOddsConfig : ScriptableObject
 {
-    public AnimationCurve TestCurve;
     public float RandomRangeOffset = 1.5f;
     public List<CholeraThresholdOddModel> ListOfThresholds;
 }
