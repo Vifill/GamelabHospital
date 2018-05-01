f7d75a8e5c99e66cc6fdfd314abac7cf043a5eaf
Vífill Valdimarsson <vifill12@gmail.com>
2018-05-01 12:31:28 +0200

    A little tuning of the excrete time config

--- Assets/_Scripts/ScriptableObjects/CholeraConfigs/CholeraThresholdOddsConfig.cs
+++ Assets/_Scripts/ScriptableObjects/CholeraConfigs/CholeraThresholdOddsConfig.cs
@@ -4,7 +4,8 @@ using System.Collections.Generic;
 
 [CreateAssetMenu(menuName = "Configs/Patient/CholeraThresholdOddsConfig")]
 public class CholeraThresholdOddsConfig : ScriptableObject
-{ 
+{
+    public AnimationCurve TestCurve;
     public float RandomRangeOffset = 1.5f;
     public List<CholeraThresholdOddModel> ListOfThresholds;
 }
