0afa4a7e0103cc0f5e08715796c366a09d32977d
Theodor Te Maiharoa <tedditm@gmail.com>
2018-05-02 17:27:38 +0200

    tutorial fixes, fixing the enum's thing and having same objective text for both levels

--- Assets/_Scripts/Tutorial/NewTutorial/EventManager.cs
+++ Assets/_Scripts/Tutorial/NewTutorial/EventManager.cs
@@ -93,7 +93,7 @@ public class EventManager : MonoBehaviour
         DoneCleanBucket,
         DoneReturnBucket,
         DoneCleanDoctor,
-        DoneFinishingTutorialQueue
+        DoneFinishingTutorialQueue,
         DonePuking
     }
 }
