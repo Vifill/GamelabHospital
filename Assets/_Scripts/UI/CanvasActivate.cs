0dd37b973390f79dd2ebb2bda75f45ac6fd3f8cb
Per Kristian Sørensen <Peks_96@live.no>
2018-05-10 12:18:40 +0200

    LightPrefabAdded

---
+++ Assets/_Scripts/UI/CanvasActivate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class CanvasActivate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    gameObject.SetActive(true);
+	}
+}
