3e97fffe1ac807038a774f59cbee1d62489f200c
Sindri Freyr <sindridi@gmail.com>
2018-05-15 16:23:50 +0200

    door animation finally working for everything

--- Assets/_Scripts/DoorController.cs
+++ Assets/_Scripts/DoorController.cs
@@ -1,12 +1,14 @@
 ﻿using Assets._Scripts.Utilities;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DoorController : MonoBehaviour 
 {
     public Animator Animator;
-    private bool DontClose;
+    private bool IsOpen;
+    private List<GameObject> PeopleAtDoor = new List<GameObject>();
 
 	private void Start() 
 	{
@@ -20,10 +22,37 @@ public class DoorController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        if (other.tag == "Player")
+        {
+            print("poopy player door enter");
+        }
+
         if (other.tag == "Patient" || other.tag == "Player" && LevelManager.TimeOver)
         {
-            print("Door trigger activated");
-            Animator.SetTrigger(Constants.AnimationParameters.DoorOpen);
+            PeopleAtDoor.Add(other.gameObject);
+            if (!IsOpen)
+            {
+                Animator.SetBool(Constants.AnimationParameters.DoorOpen, true);
+                IsOpen = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            print("poopy player door exit");
+        }
+
+        if (other.tag == "Patient" || other.tag == "Player" && LevelManager.TimeOver)
+        {
+            PeopleAtDoor.Remove(other.gameObject);
+            if (IsOpen && !PeopleAtDoor.Any())
+            {
+                Animator.SetBool(Constants.AnimationParameters.DoorOpen, false);
+                IsOpen = false;
+            }
         }
     }
 }
