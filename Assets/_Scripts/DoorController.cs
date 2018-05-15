2daeb475d0da9853d89612355303ea4b20b4e035
Sindri Freyr <sindridi@gmail.com>
2018-05-15 13:21:05 +0200

    open door animation

---
+++ Assets/_Scripts/DoorController.cs
@@ -0,0 +1,29 @@
+﻿using Assets._Scripts.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour 
+{
+    public Animator Animator;
+    private bool DontClose;
+
+	private void Start() 
+	{
+
+	}
+	
+	private void Update() 
+	{
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Patient")
+        {
+            print("Door trigger activated");
+            Animator.SetTrigger(Constants.AnimationParameters.DoorOpen);
+        }
+    }
+}
